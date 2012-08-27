4f883063dde4267ac4f6cfa9ac9abb4b7fed3e72
Amaan Cheval <amaan.1996@gmail.com>
2012-08-27 13:33:40 +0530

    Update SOUpdateQuestions/SODotUpdate.user.js

--- SOUpdateQuestions/SODotUpdate.user.js
+++ SOUpdateQuestions/SODotUpdate.user.js
@@ -1,6 +1,8 @@
 document.body.addEventListener('keyup', function (e){
     if(e.keyCode === 190){
         var newPost = document.getElementsByClassName('new-post-activity');
-        newPost[0].click();
+        if(newPost){
+            newPost[0].click();
+        }
     }
 }, false);
