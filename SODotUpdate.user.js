813f6d6d16c175ece46e2158baf340a2117e9734
Amaan Cheval <amaan.cheval@gmail.com>
2015-06-02 15:25:06 +0530

    Added meta-data

--- SODotUpdate.user.js
+++ SODotUpdate.user.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name          StackOverflow Update
+// @description   Lets you press "." to refresh questions the way Twitter does it
+// @include       *://*.stackoverflow.com/*
+// @version       1.0
+// ==/UserScript==
+
 document.body.addEventListener('keyup', function (e){
     if(e.keyCode === 190){
         var newPost = document.getElementsByClassName('new-post-activity');
