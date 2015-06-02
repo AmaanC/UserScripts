697cb43651b8a9f30ce942cd9964a0f6c94719d2
Amaan Cheval <amaan.cheval@gmail.com>
2015-06-02 15:23:17 +0530

    Added Duolingo userscript

---
+++ duolingo.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name          Duolingo
+// @description   Lets you use alt+up or alt+down to scroll down to your next lesson, or up to the top of the page
+// @include       *://*.duolingo.com/*
+// @version       1.0
+// ==/UserScript==
+
+
+var down = function() {
+    [].pop.apply(document.querySelectorAll('.gold')).scrollIntoView();
+};
+
+var up = function() {
+    window.scrollTo(0, 0);
+};
+
+
+document.body.addEventListener('keydown', function (e) {
+    if (e.altKey) {
+        if (e.keyCode === 38) {
+            up();
+        }
+        else if(e.keyCode === 40) {
+            down();
+        }
+        else if(e.keyCode === 32) {
+            location.href = 'https://www.duolingo.com/practice';
+        }
+    }
+}, false);
+
+console.log('Init');
