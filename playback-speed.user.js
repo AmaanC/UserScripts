0ad40b89dad24b5f4cfe0f6cfaa073db3b5ebf59
Amaan Cheval <amaan.cheval@gmail.com>
2015-08-16 20:03:10 +0530

    Added playback userscript

---
+++ playback-speed.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name          Playback Rate changer
+// @description   Lets you use alt and + or alt and - to change the playback rate of HTML5 video elements
+// @include       *://*/*
+// @version       1.0
+// ==/UserScript==
+
+var init = function() {
+    console.log('Playback init');
+    if (!localStorage.speed) {
+        localStorage.speed = '1';
+    }
+    document.body.addEventListener('load', function() {
+        changeSpeedBy(0);
+    });
+};
+
+var changeSpeedBy = function(delta) {
+    localStorage.speed = +(localStorage.speed) + delta;
+    console.log('New speed:', localStorage.speed);
+    var videoElem = document.querySelector('video');
+    if (videoElem) {
+        videoElem.playbackRate = localStorage.speed;
+    }
+    else {
+        console.error('No video element.');
+    }
+};
+
+document.body.addEventListener('keydown', function (e) {
+    if (e.altKey) {
+        if (e.keyCode === 189) {
+            // Reduce speed
+            changeSpeedBy(-0.25);
+        }
+        else if(e.keyCode === 187) {
+            // Increase speed
+            changeSpeedBy(+0.25);
+        }
+    }
+}, false);
+
+init();
